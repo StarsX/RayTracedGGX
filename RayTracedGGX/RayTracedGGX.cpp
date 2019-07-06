3a1ffe30d6f0b3b3f46c7826a79540ae7450c195
Tianchen Xu <TianchenX@outlook.com>
2019-07-06 20:02:08 +0800

    Enable DXR was missing.

--- RayTracedGGX/RayTracedGGX.cpp
+++ RayTracedGGX/RayTracedGGX.cpp
@@ -72,6 +72,7 @@ void RayTracedGGX::LoadPipeline()
 	{
 		dxgiAdapter = nullptr;
 		ThrowIfFailed(factory->EnumAdapters1(i, &dxgiAdapter));
+		EnableDirectXRaytracing(dxgiAdapter.get());
 		hr = D3D12CreateDevice(dxgiAdapter.get(), D3D_FEATURE_LEVEL_11_0, IID_PPV_ARGS(&m_device.Common));
 	}
 
