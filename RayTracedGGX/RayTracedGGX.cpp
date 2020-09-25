6d23b2e1278522787624c34439e0a7aa2dccf9b4
StarsX <X-Universe@outlook.com>
2020-09-25 09:57:16 +0800

    Update XUSG with stencil SRV fix.

--- RayTracedGGX/RayTracedGGX.cpp
+++ RayTracedGGX/RayTracedGGX.cpp
@@ -104,7 +104,7 @@ void RayTracedGGX::LoadPipeline()
 	swapChainDesc.BufferCount = FrameCount;
 	swapChainDesc.Width = m_width;
 	swapChainDesc.Height = m_height;
-	swapChainDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
+	swapChainDesc.Format = GetDXGIFormat(Format::R8G8B8A8_UNORM);
 	swapChainDesc.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
 	swapChainDesc.SwapEffect = DXGI_SWAP_EFFECT_FLIP_DISCARD;
 	swapChainDesc.SampleDesc.Count = 1;
