c2d3e10a61696cb53a903ba73e7adb65605d5ee2
StarsX <X-Universe@outlook.com>
2022-07-27 09:00:26 +0800

    Update D3D12 headers and include local headers

--- RayTracedGGX/stdafx.h
+++ RayTracedGGX/stdafx.h
@@ -23,10 +23,10 @@
 
 #include <windows.h>
 
+#include "d3dx12.h"
 #include <dxgi1_5.h>
 #include <D3Dcompiler.h>
 #include <DirectXMath.h>
-#include "d3dx12.h"
 
 #include <iostream>
 #include <sstream>
